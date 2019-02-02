6a31655b2019634f36cb41e8c895951de4be9e3e
jfpsb <jfpsb_felipe2@hotmail.com>
2019-02-01 23:48:55 -0300

    Iniciando projeto em WPF

---
+++ VandaModaIntimaWpf/VandaModaIntima.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace VandaModaIntimaWpf
+{
+    public partial class VandaModaIntima : Window
+    {
+        public VandaModaIntima()
+        {
+            InitializeComponent();
+        }
+
+        private void Sair_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Produto_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
