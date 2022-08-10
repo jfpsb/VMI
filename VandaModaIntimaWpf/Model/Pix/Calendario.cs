697091b48589970ca1fa8b979545f4d3124b76d1
Felipe Borges <jfpsb_felipe2@hotmail.com>
2022-08-10 00:48:00 -0300

    Adicionando classes model de Pix

---
+++ VandaModaIntimaWpf/Model/Pix/Calendario.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace VandaModaIntimaWpf.Model
+{
+    public class Calendario : AModel, IModel
+    {
+        private int _id;
+        private DateTime _criacao;
+        private DateTime _apresentacao;
+        private int _expiracao;
+
+        public virtual int Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public virtual DateTime Criacao
+        {
+            get => _criacao;
+            set
+            {
+                _criacao = value;
+                OnPropertyChanged("Criacao");
+                OnPropertyChanged("CriacaoLocalTime");
+            }
+        }
+        public virtual DateTime CriacaoLocalTime
+        {
+            get => _criacao.ToLocalTime();
+        }
+        public virtual DateTime Apresentacao
+        {
+            get => _apresentacao;
+            set
+            {
+                _apresentacao = value;
+                OnPropertyChanged("Apresentacao");
+                OnPropertyChanged("ApresentacaoLocalTime");
+            }
+        }
+        public virtual DateTime ApresentacaoLocalTime
+        {
+            get => _apresentacao.ToLocalTime();
+        }
+        public virtual int Expiracao
+        {
+            get => _expiracao;
+            set
+            {
+                _expiracao = value;
+                OnPropertyChanged("Expiracao");
+            }
+        }
+
+        public string GetContextMenuHeader => throw new NotImplementedException();
+
+        public object GetIdentifier()
+        {
+            return Id;
+        }
+
+        public void InicializaLazyLoad()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VandaModaIntimaWpf/Model/Pix/Valor.cs
@@ -0,0 +1,48 @@
+﻿namespace VandaModaIntimaWpf.Model
+{
+    public class Valor : AModel, IModel
+    {
+        private int _id;
+        private double _original;
+
+        public object GetIdentifier()
+        {
+            return Id;
+        }
+
+        public void InicializaLazyLoad()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual int Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public virtual double Original
+        {
+            get
+            {
+                return _original;
+            }
+
+            set
+            {
+                _original = value;
+                OnPropertyChanged("Original");
+            }
+        }
+
+        public string GetContextMenuHeader => throw new System.NotImplementedException();
+    }
+}
