05becfec1d4e6b6ee445de46b4f00c32781b4805
Felipe Borges <jfpsb_felipe2@hotmail.com>
2022-08-10 00:37:54 -0300

    Removendo itens de tela inicial Mantendo somente botão para abrir tela de ponto Abrindo tela de registro para funcionário

--- VandaModaIntimaWpf/View/VandaModaIntima.xaml.cs
+++ VandaModaIntimaWpf/View/VandaModaIntima.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System.Windows;
+﻿using NHibernate;
+using System.Windows;
 using VandaModaIntimaWpf.BancoDeDados.ConnectionFactory;
-using VandaModaIntimaWpf.View.Avisos;
-using VandaModaIntimaWpf.View.CompraDeFornecedor;
-using VandaModaIntimaWpf.View.Contagem;
-using VandaModaIntimaWpf.View.Despesa;
-using VandaModaIntimaWpf.View.EntradaDeMercadoria;
-using VandaModaIntimaWpf.View.Ferias;
-using VandaModaIntimaWpf.View.FolhaPagamento;
-using VandaModaIntimaWpf.View.Fornecedor;
-using VandaModaIntimaWpf.View.Funcionario;
-using VandaModaIntimaWpf.View.Loja;
-using VandaModaIntimaWpf.View.Marca;
 using VandaModaIntimaWpf.View.PontoEletronico;
-using VandaModaIntimaWpf.View.Produto;
-using VandaModaIntimaWpf.View.RecebimentoCartao;
 using VandaModaIntimaWpf.ViewModel.PontoEletronico;
 using VandaModaIntimaWpf.ViewModel.Services.Concretos;
 
@@ -37,89 +25,18 @@ namespace VandaModaIntimaWpf.View
             SessionProvider.FechaSessionFactory();
         }
 
-        private void BtnProduto_Click(object sender, RoutedEventArgs e)
-        {
-            PesquisarProduto pesquisarProduto = new PesquisarProduto();
-            pesquisarProduto.Show();
-        }
-
-        private void BtnFornecedor_Click(object sender, RoutedEventArgs e)
-        {
-            PesquisarFornecedor pesquisarFornecedor = new PesquisarFornecedor();
-            pesquisarFornecedor.Show();
-        }
-
-        private void BtnFuncionario_Click(object sender, RoutedEventArgs e)
-        {
-            PesquisarFuncionario pesquisarFuncionario = new PesquisarFuncionario();
-            pesquisarFuncionario.Show();
-        }
-
-        private void BtnLoja_Click(object sender, RoutedEventArgs e)
-        {
-            PesquisarLoja pesquisarLoja = new PesquisarLoja();
-            pesquisarLoja.Show();
-        }
-
-        private void BtnMarca_Click(object sender, RoutedEventArgs e)
-        {
-            PesquisarMarca pesquisarMarca = new PesquisarMarca();
-            pesquisarMarca.Show();
-        }
-
-        private void BtnCompraDeFornecedor_Click(object sender, RoutedEventArgs e)
-        {
-            PesquisarCompraDeFornecedor pesquisarCompraDeFornecedor = new PesquisarCompraDeFornecedor();
-            pesquisarCompraDeFornecedor.Show();
-        }
-
-        private void BtnRecebimentoCartao_Click(object sender, RoutedEventArgs e)
-        {
-            PesquisarRecebimento pesquisarRecebimento = new PesquisarRecebimento();
-            pesquisarRecebimento.Show();
-        }
-
-        private void BtnContagem_Click(object sender, RoutedEventArgs e)
-        {
-            PesquisarContagem pesquisarContagem = new PesquisarContagem();
-            pesquisarContagem.Show();
-        }
-
-        private void BtnDespesa_Click(object sender, RoutedEventArgs e)
-        {
-            PesquisarDespesa pesquisarDespesa = new PesquisarDespesa();
-            pesquisarDespesa.Show();
-        }
-
-        private void BtnFolhaPagamento_Click(object sender, RoutedEventArgs e)
-        {
-            PesquisarFolhaPagamento pesquisarFolhaPagamento = new PesquisarFolhaPagamento();
-            pesquisarFolhaPagamento.Show();
-        }
-
-        private void BtnEntradaDeMercadoria_Click(object sender, RoutedEventArgs e)
-        {
-            PesquisarEntradaDeMercadoria pesquisarEntradaDeMercadoria = new PesquisarEntradaDeMercadoria();
-            pesquisarEntradaDeMercadoria.Show();
-        }
-
-        private void BtnFerias_Click(object sender, RoutedEventArgs e)
-        {
-            VisualizadorDeFerias visualizadorDeFerias = new VisualizadorDeFerias();
-            visualizadorDeFerias.Show();
-        }
-
         private void TelaPrincipal_Loaded(object sender, RoutedEventArgs e)
         {
-            TelaDeAviso telaDeAviso = new TelaDeAviso();
-            telaDeAviso.Owner = this;
-            telaDeAviso.ShowDialog();
+
         }
 
         private void BtnPontoEletronico_Click(object sender, RoutedEventArgs e)
         {
-            PesquisarPontoEletronico pesquisarPonto = new PesquisarPontoEletronico();
-            pesquisarPonto.Show();
+            ISession session = SessionProvider.GetSession();
+            new WindowService().ShowDialog(new RegistrarPontoVM(session), (result, vm) =>
+            {
+                SessionProvider.FechaSession(session);
+            });
         }
     }
 }
--- VandaModaIntimaWpf/ViewModel/VandaModaIntimaVM.cs
+++ VandaModaIntimaWpf/ViewModel/VandaModaIntimaVM.cs
@@ -5,42 +5,9 @@ using System.IO;
 using System.Windows;
 using VandaModaIntimaWpf.BancoDeDados.ConnectionFactory;
 using VandaModaIntimaWpf.Model;
-using VandaModaIntimaWpf.View;
-using VandaModaIntimaWpf.View.Avisos;
-using VandaModaIntimaWpf.View.CompraDeFornecedor;
-using VandaModaIntimaWpf.View.Contagem;
-using VandaModaIntimaWpf.View.Despesa;
-using VandaModaIntimaWpf.View.EntradaDeMercadoria;
-using VandaModaIntimaWpf.View.FolhaPagamento;
-using VandaModaIntimaWpf.View.FolhaPagamento.CalculoDeBonusMensalPorDia;
-using VandaModaIntimaWpf.View.Fornecedor;
-using VandaModaIntimaWpf.View.Funcionario;
-using VandaModaIntimaWpf.View.Grade;
-using VandaModaIntimaWpf.View.Loja;
-using VandaModaIntimaWpf.View.Marca;
 using VandaModaIntimaWpf.View.PontoEletronico;
-using VandaModaIntimaWpf.View.Produto;
-using VandaModaIntimaWpf.View.RecebimentoCartao;
-using VandaModaIntimaWpf.View.Representante;
-using VandaModaIntimaWpf.View.TipoGrade;
-using VandaModaIntimaWpf.ViewModel.Avisos;
-using VandaModaIntimaWpf.ViewModel.CompraDeFornecedor;
-using VandaModaIntimaWpf.ViewModel.Contagem;
-using VandaModaIntimaWpf.ViewModel.Despesa;
-using VandaModaIntimaWpf.ViewModel.EntradaDeMercadoria;
-using VandaModaIntimaWpf.ViewModel.FolhaPagamento;
-using VandaModaIntimaWpf.ViewModel.FolhaPagamento.CalculoDeBonusMensalPorDia;
-using VandaModaIntimaWpf.ViewModel.Fornecedor;
-using VandaModaIntimaWpf.ViewModel.Funcionario;
-using VandaModaIntimaWpf.ViewModel.Grade;
-using VandaModaIntimaWpf.ViewModel.Loja;
-using VandaModaIntimaWpf.ViewModel.Marca;
 using VandaModaIntimaWpf.ViewModel.PontoEletronico;
-using VandaModaIntimaWpf.ViewModel.Produto;
-using VandaModaIntimaWpf.ViewModel.RecebimentoCartao;
-using VandaModaIntimaWpf.ViewModel.Representante;
 using VandaModaIntimaWpf.ViewModel.Services.Concretos;
-using VandaModaIntimaWpf.ViewModel.TipoGrade;
 
 namespace VandaModaIntimaWpf.ViewModel
 {
@@ -48,71 +15,7 @@ namespace VandaModaIntimaWpf.ViewModel
     {
         public VandaModaIntimaVM()
         {
-            WindowService.RegistrarWindow<SalvarCompraDeFornecedor, CadastrarCompraDeFornecedorVM>();
-            WindowService.RegistrarWindow<SalvarCompraDeFornecedor, EditarCompraDeFornecedorVM>();
-            WindowService.RegistrarWindow<CadastrarContagem, CadastrarContagemVM>();
-            WindowService.RegistrarWindow<EditarContagem, EditarContagemVM>();
-            WindowService.RegistrarWindow<SalvarDespesa, CadastrarDespesaVM>();
-            WindowService.RegistrarWindow<SalvarDespesa, EditarDespesaVM>();
-            WindowService.RegistrarWindow<SalvarEntradaDeMercadoria, CadastrarEntradaDeMercadoriaVM>();
-            WindowService.RegistrarWindow<SelecionaMultiplasLojas, SelecionaMultiplasLojasVM>();
-
-            WindowService.RegistrarWindow<CalculoBonusMensalPorDiaView, CalculoDeBonusMensalPorDiaVM>();
-            WindowService.RegistrarWindow<SalvarBonusDeFuncionario, SalvarBonusPorMesVM>();
-
-            WindowService.RegistrarWindow<AdicionarAdiantamento, AdicionarAdiantamentoVM>();
-            WindowService.RegistrarWindow<AdicionarBonus, AdicionarBonusVM>();
-            WindowService.RegistrarWindow<AdicionarFaltas, AdicionarFaltasVM>();
-            WindowService.RegistrarWindow<AdicionarHoraExtra, AdicionarHoraExtraVM>();
-            WindowService.RegistrarWindow<AdicionarMetaIndividual, AdicionarMetaIndividualVM>();
-            WindowService.RegistrarWindow<AdicionarObservacaoFolha, AdicionarObservacaoFolhaVM>();
-            WindowService.RegistrarWindow<AdicionarSalarioLiquido, AdicionarSalarioLiquidoVM>();
-            WindowService.RegistrarWindow<AdicionarTotalVendido, AdicionarTotalVendidoVM>();
-            WindowService.RegistrarWindow<GerenciarParcelas, GerenciarParcelasVM>();
-            WindowService.RegistrarWindow<View.FolhaPagamento.MaisDetalhes, FolhaPagamento.MaisDetalhesVM>();
-            WindowService.RegistrarWindow<VisualizarHoraExtraFaltas, VisualizarHoraExtraFaltasVM>();
-            WindowService.RegistrarWindow<VisualizarDadosBancarios, VisualizarDadosBancariosVM>();
-
-            WindowService.RegistrarWindow<SalvarFornecedor, CadastrarFornecedorManualmenteVM>();
-            WindowService.RegistrarWindow<SalvarFornecedor, CadastrarFornecedorOnlineVM>();
-            WindowService.RegistrarWindow<SalvarFornecedor, EditarFornecedorVM>();
-
-            WindowService.RegistrarWindow<SalvarFuncionario, CadastrarFuncionarioVM>();
-            WindowService.RegistrarWindow<SalvarFuncionario, EditarFuncionarioVM>();
-
-            WindowService.RegistrarWindow<CadastrarGrade, CadastrarGradeVM>();
-
-            WindowService.RegistrarWindow<SalvarLoja, CadastrarLojaVM>();
-            WindowService.RegistrarWindow<SalvarLoja, EditarLojaVM>();
-
-            WindowService.RegistrarWindow<CadastrarMarca, CadastrarMarcaVM>();
-
-            WindowService.RegistrarWindow<SalvarProduto, CadastrarProdutoVM>();
-            WindowService.RegistrarWindow<SalvarProduto, EditarProdutoVM>();
-
-            WindowService.RegistrarWindow<CadastrarRecebimentoCartao, CadastrarRecebimentoVM>();
-            WindowService.RegistrarWindow<View.RecebimentoCartao.MaisDetalhes, RecebimentoCartao.MaisDetalhesVM>();
-
-            WindowService.RegistrarWindow<SalvarRepresentante, CadastrarRepresentanteVM>();
-            WindowService.RegistrarWindow<SalvarRepresentante, EditarRepresentanteVM>();
-
-            WindowService.RegistrarWindow<CadastrarTipoGrade, CadastrarTipoGradeVM>();
-
-            WindowService.RegistrarWindow<TelaDeAviso, TelaDeAvisoVM>();
-
             WindowService.RegistrarWindow<RegistrarPonto, RegistrarPontoVM>();
-
-            WindowService.RegistrarWindow<TelaDeLoginSimples, InserirSenhaPontoVM>();
-
-            WindowService.RegistrarWindow<SalvarEntradaDeMercadoria, EditarEntradaDeMercadoriaVM>();
-
-            WindowService.RegistrarWindow<TrocarSenhaFuncionario, TrocarSenhaFuncionarioVM>();
-
-            WindowService.RegistrarWindow<PesquisarRepresentante, PesquisarRepresentanteVM>();
-
-            WindowService.RegistrarWindow<ConsolidarPontosEletronicos, ConsolidarPontosEletronicosVM>();
-
-            WindowService.RegistrarWindow<ConfirmarConsolidacaoPontosEletronicos, ConfirmarConsolidacaoPontosEletronicosVM>();
 
             try
             {
