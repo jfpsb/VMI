0feebea8dc65d73866ce634681b0d59d05fa4a55
Felipe Borges <jfpsb_felipe2@hotmail.com>
2022-08-29 15:24:18 -0300

    Arrumando tela inicial

--- VandaModaIntimaWpf/View/VandaModaIntima.xaml.cs
+++ VandaModaIntimaWpf/View/VandaModaIntima.xaml.cs
@@ -1,5 +1,4 @@
-﻿using NHibernate;
-using System.Windows;
+﻿using System.Windows;
 using VandaModaIntimaWpf.BancoDeDados.ConnectionFactory;
 using VandaModaIntimaWpf.View.Avisos;
 using VandaModaIntimaWpf.View.CompraDeFornecedor;
@@ -14,6 +13,8 @@ using VandaModaIntimaWpf.View.Loja;
 using VandaModaIntimaWpf.View.Marca;
 using VandaModaIntimaWpf.View.Pix;
 using VandaModaIntimaWpf.View.PontoEletronico;
+using VandaModaIntimaWpf.View.Produto;
+using VandaModaIntimaWpf.View.RecebimentoCartao;
 using VandaModaIntimaWpf.ViewModel.PontoEletronico;
 using VandaModaIntimaWpf.ViewModel.Services.Concretos;
 
@@ -37,24 +38,95 @@ namespace VandaModaIntimaWpf.View
             SessionProvider.FechaSessionFactory();
         }
 
+        private void BtnProduto_Click(object sender, RoutedEventArgs e)
+        {
+            PesquisarProduto pesquisarProduto = new PesquisarProduto();
+            pesquisarProduto.Show();
+        }
+
+        private void BtnFornecedor_Click(object sender, RoutedEventArgs e)
+        {
+            PesquisarFornecedor pesquisarFornecedor = new PesquisarFornecedor();
+            pesquisarFornecedor.Show();
+        }
+
+        private void BtnFuncionario_Click(object sender, RoutedEventArgs e)
+        {
+            PesquisarFuncionario pesquisarFuncionario = new PesquisarFuncionario();
+            pesquisarFuncionario.Show();
+        }
+
+        private void BtnLoja_Click(object sender, RoutedEventArgs e)
+        {
+            PesquisarLoja pesquisarLoja = new PesquisarLoja();
+            pesquisarLoja.Show();
+        }
+
+        private void BtnMarca_Click(object sender, RoutedEventArgs e)
+        {
+            PesquisarMarca pesquisarMarca = new PesquisarMarca();
+            pesquisarMarca.Show();
+        }
+
+        private void BtnCompraDeFornecedor_Click(object sender, RoutedEventArgs e)
+        {
+            PesquisarCompraDeFornecedor pesquisarCompraDeFornecedor = new PesquisarCompraDeFornecedor();
+            pesquisarCompraDeFornecedor.Show();
+        }
+
+        private void BtnRecebimentoCartao_Click(object sender, RoutedEventArgs e)
+        {
+            PesquisarRecebimento pesquisarRecebimento = new PesquisarRecebimento();
+            pesquisarRecebimento.Show();
+        }
+
+        private void BtnContagem_Click(object sender, RoutedEventArgs e)
+        {
+            PesquisarContagem pesquisarContagem = new PesquisarContagem();
+            pesquisarContagem.Show();
+        }
+
+        private void BtnDespesa_Click(object sender, RoutedEventArgs e)
+        {
+            PesquisarDespesa pesquisarDespesa = new PesquisarDespesa();
+            pesquisarDespesa.Show();
+        }
+
+        private void BtnFolhaPagamento_Click(object sender, RoutedEventArgs e)
+        {
+            PesquisarFolhaPagamento pesquisarFolhaPagamento = new PesquisarFolhaPagamento();
+            pesquisarFolhaPagamento.Show();
+        }
+
+        private void BtnEntradaDeMercadoria_Click(object sender, RoutedEventArgs e)
+        {
+            PesquisarEntradaDeMercadoria pesquisarEntradaDeMercadoria = new PesquisarEntradaDeMercadoria();
+            pesquisarEntradaDeMercadoria.Show();
+        }
+
+        private void BtnFerias_Click(object sender, RoutedEventArgs e)
+        {
+            VisualizadorDeFerias visualizadorDeFerias = new VisualizadorDeFerias();
+            visualizadorDeFerias.Show();
+        }
+
         private void TelaPrincipal_Loaded(object sender, RoutedEventArgs e)
         {
-
+            TelaDeAviso telaDeAviso = new TelaDeAviso();
+            telaDeAviso.Owner = this;
+            telaDeAviso.ShowDialog();
         }
 
         private void BtnPontoEletronico_Click(object sender, RoutedEventArgs e)
         {
-            ISession session = SessionProvider.GetSession();
-            new WindowService().ShowDialog(new RegistrarPontoVM(session), (result, vm) =>
-            {
-                SessionProvider.FechaSession(session);
-            });
+            PesquisarPontoEletronico pesquisarPonto = new PesquisarPontoEletronico();
+            pesquisarPonto.Show();
         }
 
         private void BtnPix_Click(object sender, RoutedEventArgs e)
         {
-            PagamentoPix pagamentoPix = new PagamentoPix();
-            pagamentoPix.ShowDialog();
+            PesquisarPix pesquisarPix = new PesquisarPix();
+            pesquisarPix.Show();
         }
     }
 }
