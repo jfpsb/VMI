803901f763f7072e51c42297f274a001b61e8821
Felipe Borges <jfpsb_felipe2@hotmail.com>
2024-01-21 19:57:11 -0300

    Mudando leitura de csv credishop para arquivo de concialição

--- VandaModaIntimaWpf/ViewModel/VendaEmCartao/LerCSVCredishop.cs
+++ VandaModaIntimaWpf/ViewModel/VendaEmCartao/LerCSVCredishop.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using VandaModaIntimaWpf.Model;
-using VandaModaIntimaWpf.Util;
 
 namespace VandaModaIntimaWpf.ViewModel.VendaEmCartao
 {
@@ -14,78 +13,78 @@ namespace VandaModaIntimaWpf.ViewModel.VendaEmCartao
             using (var reader = new StreamReader(caminhoCSV))
             {
                 List<Model.VendaEmCartao> vendas = new();
+                Dictionary<string, Model.VendaEmCartao> dicionarioVendaEmCartao = new();
 
                 //Não possui cabeçalho
+                //Cada linha do arquivo csv de conciliação é uma parcela
 
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
                     var values = line.Split(',');
 
-                    var data = values[1];
-                    var hora = values[2].Remove(values[2].Length - 2);
-                    var rv = loja.Cnpj + values[4].TrimStart('0');
-                    var parcelas = int.Parse(values[7]);
-                    var valorBruto = values[8].Insert(values[8].Length - 2, ".");
-
-                    var status = values[6];
-
-                    if (!status.Equals("I")) //Venda estornada
-                    {
-                        continue;
-                    }
+                    var rv = loja.Cnpj + values[3].TrimStart('0');
+                    var dataVenda = values[6];
+                    var valorBrutoParcela = Convert.ToDouble(values[7], CultureInfo.InvariantCulture);
+                    var dataVencimento = DateTime.ParseExact(values[8], "dd/MM/yyyy", new CultureInfo("pt-BR")); //Data que a parcela é paga
+                    var totalParcelas = Convert.ToInt32(values[5].Substring(2));
 
                     var modalidade = "CRÉDITO";
                     var bandeira = "CREDISHOP";
 
+                    Model.VendaEmCartao vendaEmCartao;
 
-                    Model.VendaEmCartao vendaEmCartao = new Model.VendaEmCartao();
-
-                    vendaEmCartao.DataHora = DateTime.ParseExact($"{data} {hora}", "yyyyMMdd HHmmss", new CultureInfo("pt-BR"));
-                    vendaEmCartao.ValorBruto = double.Parse(valorBruto, NumberStyles.Any, CultureInfo.InvariantCulture);
-
-                    if (parcelas > 1)
+                    if (dicionarioVendaEmCartao.ContainsKey(rv))
                     {
-                        vendaEmCartao.ValorLiquido = vendaEmCartao.ValorBruto * 0.955; //taxa 4,5%
+                        //Se for parcela de uma venda já lida antes, recupero do dicionário
+                        vendaEmCartao = dicionarioVendaEmCartao[rv];
                     }
                     else
                     {
-                        vendaEmCartao.ValorLiquido = vendaEmCartao.ValorBruto * 0.96; //taxa 4.0%
-                    }
-
-                    vendaEmCartao.Modalidade = modalidade.ToUpper();
-                    vendaEmCartao.Bandeira = bandeira.ToUpper();
-                    vendaEmCartao.RvCredishop = rv;
-                    vendaEmCartao.Loja = loja;
-                    vendaEmCartao.OperadoraCartao = operadora;
-
-                    for (int i = 0; i < parcelas; i++)
-                    {
-                        //Data de pagamento de parcela é sempre calculada a partir da data original da compra, independente da data de parcela anterior
-                        //De acordo com a credishop são 32 dias de prazo para crédito para cada parcela
-                        DateTime dataPagamentoParcelaOriginal = vendaEmCartao.DataHora.Date.AddDays((i + 1) * 32);
-                        DateTime dataPagamentoParcela = dataPagamentoParcelaOriginal;
-
-                        ParcelaCartao parcelaCartao = new ParcelaCartao();
-                        parcelaCartao.VendaEmCartao = vendaEmCartao;
-                        parcelaCartao.ValorBruto = vendaEmCartao.ValorBruto / parcelas;
-                        parcelaCartao.ValorLiquido = vendaEmCartao.ValorLiquido / parcelas;
-
-                        //Se data de pagamento cair em dia de sábado, domingo ou feriado nacional, adiciono um dia até que encontre
-                        //um dia útil
-                        while (dataPagamentoParcela.DayOfWeek == DayOfWeek.Saturday ||
-                            dataPagamentoParcela.DayOfWeek == DayOfWeek.Sunday ||
-                            FeriadoJsonUtil.IsDataFeriadoNacional(dataPagamentoParcela))
+                        //Se for parcela de venda ainda não lida, crio o objeto de venda em cartão
+                        vendaEmCartao = new Model.VendaEmCartao
                         {
-                            dataPagamentoParcela = dataPagamentoParcela.AddDays(1);
+                            DataHora = DateTime.ParseExact($"{dataVenda}", "dd/MM/yyyy", new CultureInfo("pt-BR")),
+                            ValorBruto = valorBrutoParcela * totalParcelas,
+                            Modalidade = modalidade.ToUpper(),
+                            Bandeira = bandeira.ToUpper(),
+                            RvCredishop = rv,
+                            Loja = loja,
+                            OperadoraCartao = operadora
+                        };
+
+                        if (totalParcelas > 1)
+                        {
+                            vendaEmCartao.ValorLiquido = vendaEmCartao.ValorBruto * 0.955; //taxa 4,5%
+                        }
+                        else
+                        {
+                            vendaEmCartao.ValorLiquido = vendaEmCartao.ValorBruto * 0.96; //taxa 4.0%
                         }
 
-                        parcelaCartao.DataPagamento = dataPagamentoParcela;
-
-                        vendaEmCartao.Parcelas.Add(parcelaCartao);
+                        dicionarioVendaEmCartao.Add(rv, vendaEmCartao);
                     }
 
-                    vendas.Add(vendaEmCartao);
+                    ParcelaCartao parcelaCartao = new ParcelaCartao();
+                    parcelaCartao.VendaEmCartao = vendaEmCartao;
+                    parcelaCartao.DataPagamento = dataVencimento;
+                    parcelaCartao.ValorBruto = valorBrutoParcela;
+
+                    if (totalParcelas > 1)
+                    {
+                        parcelaCartao.ValorLiquido = parcelaCartao.ValorBruto * 0.955; //taxa 4,5%
+                    }
+                    else
+                    {
+                        parcelaCartao.ValorLiquido = parcelaCartao.ValorBruto * 0.96; //taxa 4.0%
+                    }
+
+                    vendaEmCartao.Parcelas.Add(parcelaCartao);
+                }
+
+                foreach (var item in dicionarioVendaEmCartao)
+                {
+                    vendas.Add(item.Value);
                 }
 
                 return vendas;
